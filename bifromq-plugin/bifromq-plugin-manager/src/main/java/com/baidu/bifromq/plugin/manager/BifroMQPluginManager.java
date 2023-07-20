b4158e8350d4ddcc98184beb314e741ee947094c
Gu Jiawei <gujiawei01@baidu.com>
2023-07-20 17:07:08 +0800

    switch contextClassLoader for extension instantiation (#17)
    this is a mitigation of ClassNotFoundException issue which may occur plugin development

--- bifromq-plugin/bifromq-plugin-manager/src/main/java/com/baidu/bifromq/plugin/manager/BifroMQPluginManager.java
+++ bifromq-plugin/bifromq-plugin-manager/src/main/java/com/baidu/bifromq/plugin/manager/BifroMQPluginManager.java
@@ -13,10 +13,14 @@
 
 package com.baidu.bifromq.plugin.manager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.pf4j.CompoundPluginLoader;
 import org.pf4j.DefaultPluginManager;
+import org.pf4j.ExtensionFactory;
 import org.pf4j.PluginLoader;
+import org.pf4j.PluginRuntimeException;
 
+@Slf4j
 public class BifroMQPluginManager extends DefaultPluginManager {
     @Override
     protected PluginLoader createPluginLoader() {
@@ -24,5 +28,26 @@ public class BifroMQPluginManager extends DefaultPluginManager {
             .add(new BifroMQDevelopmentPluginLoader(this), this::isDevelopment)
             .add(new BifroMQJarPluginLoader(this), this::isNotDevelopment)
             .add(new BifroMQDefaultPluginLoader(this), this::isNotDevelopment);
+    }
+
+    @Override
+    protected ExtensionFactory createExtensionFactory() {
+        return new ExtensionFactory() {
+            @Override
+            public <T> T create(Class<T> extensionClass) {
+                log.debug("Create instance for extension '{}'", extensionClass.getName());
+                ClassLoader originalLoader = Thread.currentThread().getContextClassLoader();
+                try {
+                    ClassLoader targetLoader = extensionClass.getClassLoader();
+                    Thread.currentThread().setContextClassLoader(targetLoader);
+                    T instance = extensionClass.newInstance();
+                    Thread.currentThread().setContextClassLoader(originalLoader);
+                    return instance;
+                } catch (Exception e) {
+                    Thread.currentThread().setContextClassLoader(originalLoader);
+                    throw new PluginRuntimeException(e);
+                }
+            }
+        };
     }
 }
