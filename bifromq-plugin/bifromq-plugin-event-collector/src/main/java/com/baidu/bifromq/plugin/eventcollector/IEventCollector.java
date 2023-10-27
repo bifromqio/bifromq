f25eebe8dfcda3ef497ba6c877a03c1b8e55ca1d
Yonny Hao <popduke@gmail.com>
2023-10-27 12:14:47 +0800

    Fix javadoc build break which is causing maven central deploy issue

--- bifromq-plugin/bifromq-plugin-event-collector/src/main/java/com/baidu/bifromq/plugin/eventcollector/IEventCollector.java
+++ bifromq-plugin/bifromq-plugin-event-collector/src/main/java/com/baidu/bifromq/plugin/eventcollector/IEventCollector.java
@@ -18,9 +18,9 @@ import org.pf4j.ExtensionPoint;
 public interface IEventCollector extends ExtensionPoint {
     /**
      * Implement this method to receive various events at runtime.
-     * <p/>
-     * Note: To reduce memory pressure, the argument event object will be reused in later call,
-     * so the ownership is not transferred to the method implementation. Make a clone if needed.
+     * <p>
+     * Note: To reduce memory pressure, the argument event object will be reused in later call, so the ownership is not
+     * transferred to the method implementation. Make a clone if needed.
      *
      * @param event the event reported
      */
