5ced02777caf85d729f19e5bc54e29ea758e21d8
Yonny Hao <popduke@gmail.com>
2023-12-20 15:24:17 +0800

    add CLA and update files with more contributor friendly headers

--- build/build-bifromq-starters/bin/bifromq-start.sh
+++ build/build-bifromq-starters/bin/bifromq-start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #
-# Copyright (c) 2023. Baidu, Inc. All Rights Reserved.
+# Copyright (c) 2023. The BifroMQ Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
