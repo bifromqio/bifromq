880a5fd872fcff7e729e00b8b9a28eba2b2653f7
Gu Jiawei <gujiaweijoe@foxmail.com>
2024-12-11 22:34:22 +0800

    1. get plugin variables from env var; if not found, fall back to system properties, and finally use default values. 2. make demo plugin to reside in a single directory.

---
+++ build/build-plugin-demo/src/main/java/com/baidu/demo/plugin/util/VariableUtil.java
@@ -0,0 +1,29 @@
+package com.baidu.demo.plugin.util;
+
+public class VariableUtil {
+    private static final String PLUGIN_PROMETHEUS_PORT = "PLUGIN_PROMETHEUS_PORT";
+    private static final String PLUGIN_PROMETHEUS_CONTEXT = "PLUGIN_PROMETHEUS_CONTEXT";
+
+    private static final int DEFAULT_PORT = 9090;
+    private static final String DEFAULT_CONTEXT = "/metrics";
+
+    public static int getPort() {
+        String prometheusPort = System.getenv(PLUGIN_PROMETHEUS_PORT);
+        if (prometheusPort == null) {
+            prometheusPort = System.getProperty(PLUGIN_PROMETHEUS_PORT, "9090");
+        }
+        try {
+            return Integer.parseUnsignedInt(prometheusPort);
+        } catch (Throwable e) {
+            return DEFAULT_PORT;
+        }
+    }
+
+    public static String getContext() {
+        String ctx = System.getenv(PLUGIN_PROMETHEUS_CONTEXT);
+        if (ctx == null) {
+            ctx = System.getProperty(PLUGIN_PROMETHEUS_CONTEXT, DEFAULT_CONTEXT);
+        }
+        return ctx.startsWith("/") ? ctx : "/" + ctx;
+    }
+}
