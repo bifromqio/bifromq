963a35a3e079e276efee74360643aba3ea5f09ca
Yonny Hao <popduke@gmail.com>
2025-03-26 22:18:51 +0800

    revert default elg worker threads back to half of available cores

--- build/build-bifromq-starters/src/main/java/com/baidu/bifromq/starter/config/model/mqtt/MQTTServerConfig.java
+++ build/build-bifromq-starters/src/main/java/com/baidu/bifromq/starter/config/model/mqtt/MQTTServerConfig.java
@@ -34,7 +34,7 @@ public class MQTTServerConfig {
     private int maxConnBandwidth = 512 * 1024;
     private int defaultKeepAliveSec = 300;
     private int bossELGThreads = 1;
-    private int workerELGThreads = Math.max(2, EnvProvider.INSTANCE.availableProcessors() / 4);
+    private int workerELGThreads = Math.max(2, EnvProvider.INSTANCE.availableProcessors() / 2);
 
     @JsonSetter(nulls = Nulls.SKIP)
     private TCPListenerConfig tcpListener = new TCPListenerConfig();
